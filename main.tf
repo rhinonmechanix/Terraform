dc2bac111e2ae0cbd957ede915a5400d82885e40
Prabhat Patra <prabhatpatra24@gmail.com>
2021-12-21 18:36:48 +0530

    variable

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 resource "local_file" "animal" {
   filename = "animal.txt"
-  content = "monkey"
-  file_permission = "0700" #force replacement
+  content = var.content
+}
+
+resource "local_file" "fruits" {
+  filename = "fruits.txt"
+  content = "Grapes"
+}
+
+resource "random_pet" "my-pet" {
+  prefix = var.prefix
+  separator = var.separator
+  length = var.length
+}
+
+resource "random_integer" "number" {
+  min = 1
+  max = 9
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+  default = "pet.txt"
+}
+
+variable "content" {
+  default = "i love pets"
+}
+
+variable "prefix" {
+  default = "mrs"
+}
+
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = "1"
+}
